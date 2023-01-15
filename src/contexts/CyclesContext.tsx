24371fa5ec7a4ac05fbfc3da259f9f313f5ab62e
Evander Inacio <evander.20112@hotmail.com>
2023-01-14 22:20:10 -0300

    LocalStorage :card_file_box:

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useReducer, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
+import { createContext, ReactNode, useEffect, useReducer, useState } from 'react'
 import {
   addNewCycleAction,
   interruptCurrentCycleAction,
@@ -34,12 +35,31 @@ export function CyclesContextProvider({
   const [cyclesState, dispatch] = useReducer(cyclesReducer, {
     cycles: [],
     activeCycleId: null
-  })
+  }, () => {
+    const storedStateAsJson = localStorage.getItem("@timer:cycles-state-1.0.0")
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+    if (storedStateAsJson) {
+      return JSON.parse(storedStateAsJson)
+    }
+  })
 
   const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+
+ 
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds( new Date(), new Date( activeCycle.startDate))
+    }
+    
+    return 0
+  })
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem("@timer:cycles-state-1.0.0", stateJSON)
+  }, [cyclesState])
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -20,7 +20,10 @@ export function Countdown() {
 
     if (activeCycle) {
       interval = setInterval(() => {
-        const secondsDifference = differenceInSeconds(new Date(), activeCycle.startDate)
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          new Date( activeCycle.startDate)
+        )
 
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
